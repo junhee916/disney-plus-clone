5c19507b000f8416975a848d183542c820ecb498
junhee0916 <junhee916@naver.com>
2022-09-20 17:55:13 +0900

    update disney plus clon -third

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+import Login from './components/Login';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/detail" element={<Detail/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
